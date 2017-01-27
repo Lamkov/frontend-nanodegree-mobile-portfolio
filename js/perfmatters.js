8910167a6f5d9032268d07493cdf105ef395abe4
Sergei Lamkov <lamkov@gmail.com>
2017-01-27 19:34:40 +0300

    3

--- js/perfmatters.js
+++ js/perfmatters.js
@@ -1,12 +1,15 @@
-// Measuring the Critical Rendering Path with Navigation Timing
-// https://developers.google.com/web/fundamentals/performance/critical-rendering-path/measure-crp
+
+window.addEventListener("load", function(event) {
+  logCRP();
+});
+
 (function(w,g){w['GoogleAnalyticsObject']=g;
   w[g]=w[g]||function(){(w[g].q=w[g].q||[]).push(arguments)};w[g].l=1*new Date();})(window,'ga');
-
+/*
 // Optional TODO: replace with your Google Analytics profile ID.
 ga('create', 'UA-XXXX-Y');
 ga('send', 'pageview');
-
+*/
 function logCRP() {
   var t = window.performance.timing,
     dcl = t.domContentLoadedEventStart - t.domLoading,
@@ -14,7 +17,3 @@ function logCRP() {
   var stats = document.getElementById("crp-stats");
   stats.textContent = 'DCL: ' + dcl + 'ms, onload: ' + complete + 'ms';
 }
-
-window.addEventListener("load", function(event) {
-  logCRP();
-});
